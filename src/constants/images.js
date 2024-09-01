34cf13fa55abbe998680f81f1e2c00d8976bd622
Muhammad shahzad <shehzadmemon147@gmail.com>
2024-09-01 22:13:57 +0500

    Done

--- src/constants/images.js
+++ src/constants/images.js
@@ -2,11 +2,13 @@ import Logo from "../assets/Logo.svg"
 import HeroImage from "../assets/HeroImage.svg"
 import PostImage from "../assets/post1.jpg"
 import UserImg from "../assets/post-profile.svg"
+import socialImages from "../assets/social.jpg"
 const images = {
     Logo,
     HeroImage,
     PostImage,
-    UserImg
+    UserImg,
+    socialImages
 }
 
 export default images
