2b8788e1ac2a9458b10022fe14190956bcfb7db3
Muhammad shahzad <shehzadmemon147@gmail.com>
2024-09-10 21:53:44 +0500

    Done

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,33 +1,50 @@
 import React from 'react'
 import { FaCheck } from "react-icons/fa6";
 import images from "../constants/images"
-const ArticleCard = ({className}) => {
+import stables from '../constants/stable';
+import { FaRegUserCircle } from "react-icons/fa";
+import { IoIosCloseCircleOutline } from "react-icons/io";
+
+
+const ArticleCard = ({post,className}) => {
   return (
     <div className={`rounded-xl overflow-hidden shadow-[rgba(7,_65,_210,_0.1)_0px_9px_30px] ${className}`}>
-        <img src={images.PostImage} alt="title" className='w-full object-cover object-center h-auto md:h-52
+        <img src={post.photo ? stables.upload_Folder_Base_Url + post.photo : images.PostImage} alt="title" className='w-full object-cover object-center h-auto md:h-52
         lg:h-48 xl:h-60
         '/>
         <div className='p-5'>
            <h2 className='font-roboto font-bold text-xl text-Dark-soft md:text-2xl 
             lg:text-[28px]
-           '>Future of work</h2>
-           <p className='text-Dark-light mt-3 text-sm md:text-lg'>Lorem ipsum dolor sit amet.</p>
+           '>{post.Title}</h2>
+           <p className='text-Dark-light mt-3 text-sm md:text-lg'>{post.Caption}</p>
            <div className='flex justify-between flex-nowrap items-center mt-6'>
             <div className='flex items-center gap-x-2 md:gap-x-2.5'>
-             <img src={images.UserImg} alt="" className='w-9 h-9 md:w-10 md:h-10'/>
+              {
+                post.user.avatar ? (
+                  <img src={stables.upload_Folder_Base_Url + post.user.avatar} alt="avatar" className='w-10 h-10 object-cover object-center rounded-full'/>
+                )
+                : (
+                  <FaRegUserCircle className='w-9 h-9 object-cover object-center rounded-full'/>
+                )
+              }
              <div className='flex flex-col'>
-               <h4 className='font-bold italic text-Dark-light text-sm md:text-base'>Shehzad</h4>
+               <h4 className='font-bold italic text-Dark-light text-sm md:text-base'>{post.user.name ? post.user.name : "John Doe"}</h4>
              <div className='flex items-center gap-x-2'>
-                <span className='bg-[#36B37E] bg-opacity-20 p-1.5 rounded-full w-fit'>
-                    <FaCheck className='w-1.5 h-1.5 text-[#36B37E]'/>
+                <span className={`${post.user.verified ? "bg-[#36B37E]" : "bg-red-500"} w-fit bg-opacity-20 rounded-full p-1.5`} >
+                  {
+                    post.user.verified ? <FaCheck className='w-1.5 h-1.5 text-[#36B37E]'/>
+                    : 
+                    <IoIosCloseCircleOutline className='w-1.5 h-1.5 text-red-500'/>
+                  }
                     </span>
                 <span className='italic text-Dark-light text-sm md:text-sm'>
-                    verify Writer
+                    {post.user.verified ? "Verified" : "Unverified"} Writer
                 </span>
              </div>
             </div>
             </div>
-            <span className='text-Dark-light font-bold italic text-sm md:text-base'>02 May</span>
+            <span className='text-Dark-light font-bold italic text-sm md:text-base'>
+              {new Date(post.createdAt).getDate()} {new Date(post.createdAt).toLocaleString("en-US", {month: "short"})}</span>
            </div>
         </div>
     </div>
--- src/pages/Container/Article.jsx
+++ src/pages/Container/Article.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import ArticleCard from '../../components/ArticleCard'
 import { FaArrowRight } from "react-icons/fa";
+import { useQuery } from '@tanstack/react-query';
+import { getAllPost } from '../../services/index/Post';
+import toast from 'react-hot-toast';
 
 const Article = () => {
+
+  const {data, isLoading, isError} = useQuery({
+    queryFn: () => getAllPost(),
+    queryKey: ['posts'],
+    onError:(error) => {
+      toast.error(error.message)
+    }
+  })
   return (
     <section className='flex flex-col container mx-auto  px-5 py-10'>
       <div className="flex flex-wrap md:gap-x-5 gap-y-5 pb-10">
-      <ArticleCard className='w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]'/>
-        <ArticleCard className='w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]'/>
+        {!isLoading && !isError && data?.map((post)=>(
+          <ArticleCard key={post._id} post={post} className='w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]'/>
+        ))}
       </div>
       <button className='mx-auto flex items-center gap-x-2 text-primary font-bold border-2 border-primary rounded-lg px-6 py-3'>
         <span>More Article</span>
